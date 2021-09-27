ba8e51b737fd3177ed8bcf180dbdbfcaf7d53e17
ltphuong <jackey.phuong@gmail.com>
2021-09-27 23:05:54 +0700

    first commit

---
+++ Test Suites/New Test Suite_TC_recording_01.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite_TC_recording_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9bfbdd9b-8d17-4bee-b3b5-4570f49d5765</testSuiteGuid>
+   <testCaseLink>
+      <guid>d93014e1-70d5-43e7-bf70-e64326afd563</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case_TC_recording_01</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
